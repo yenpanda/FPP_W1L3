88ec44f194aa4aef478f001aa8f6354d06ce32cb
hatc <hatc1984@gmail.com>
2017-03-06 14:10:40 -0600

    Updated
    Updated

--- Prog3/Main.java
+++ Prog3/Main.java
@@ -9,6 +9,7 @@ public static void main(String arg[]){
 		
 	int flag = 0;
 	Scanner in = new Scanner(System.in);
+	
 	while (flag==0) {
 	System.out.println("Enter 1 for format: MM/DD/YYYY");
 	System.out.println("Enter 2 for format: Month DD, YYYY");
@@ -22,12 +23,12 @@ public static void main(String arg[]){
 		
 			System.out.println("Choice: 1");
 			System.out.println("Enter Month:");
-			int mouth = in.nextInt();
+			int month = in.nextInt();
 			System.out.println("Enter Day:");
 			int day = in.nextInt();
 			System.out.println("Enter Year:");
 			int year = in.nextInt();
-			MyDate date1 = new MyDate(mouth, day, year);   
+			MyDate date1 = new MyDate(month, day, year);   
 
 			System.out.println("Do you want to Continue :");
 			String yesOrNo = in.next();
@@ -37,7 +38,7 @@ public static void main(String arg[]){
 			}
             if (yesOrNo.equals("N")||yesOrNo.equals("n")) {
 				flag = 1;
-				System.out.println("Terminated");
+				System.out.println("Finished");
 				break;
 			}
 			
@@ -45,22 +46,21 @@ public static void main(String arg[]){
 			
 			System.out.println("Choice: 2");
 			System.out.println("Enter Month:");
-			String MM = in.next();
+			String monthMM = in.next();
 			System.out.println("Enter Day:");
-			int DD = in.nextInt();
+			int dayDD = in.nextInt();
 			System.out.println("Enter Year:");
-			int YYYY = in.nextInt();
-			MyDate date2 = new MyDate(MM, DD, YYYY); 
+			int yearYYYY = in.nextInt();
+			MyDate date2 = new MyDate(monthMM, dayDD, yearYYYY); 
 			System.out.println("Do you want to Continue :");
 			String yesOrNo2 = in.next();
-
 	
 			if (yesOrNo2.equals("Y")||yesOrNo2.equals("y")) {
 				break;
 			}
             if (yesOrNo2.equals("N")||yesOrNo2.equals("n")) {
 				flag =1;
-				System.out.println("Terminated");
+				System.out.println("Finished");
 				break;
 			}
 			
@@ -69,31 +69,32 @@ public static void main(String arg[]){
 			System.out.println("Choice: 3");
 			
 			System.out.println("Enter Day:");
-			int DDD = in.nextInt();
+			int dayD = in.nextInt();
 			System.out.println("Enter Year:");
-			int YYY = in.nextInt();
-			MyDate date3 = new MyDate(DDD,YYY); 
+			int yearY = in.nextInt();
+			MyDate date3 = new MyDate(dayD,yearY); 
 			System.out.println("Do you want to Continue :");
 			String yesOrNo3 = in.next();
 
-		
 			if (yesOrNo3.equals("Y")||yesOrNo3.equals("y")) {
 				break;
 			}
             if (yesOrNo3.equals("N")||yesOrNo3.equals("n")) {
 				flag=1;
-				System.out.println("Terminated");
+				System.out.println("Finished");
 				break;
 			}
-		case 4:   
-			System.out.println("exit");
+		case 4:  
+			flag=1;
+
+			System.out.println("Exit");
 			break;
 		default:
-			System.out.println("Terminated");
+			flag=1;
+
+			System.out.println("Finished");
 			break;
-		}
-		
-		
+		}		
 	}
   }
 }
--- Prog3/MyDate.java
+++ Prog3/MyDate.java
@@ -5,24 +5,44 @@ import java.time.format.DateTimeFormatter;
 
 public class MyDate {
 	
-	private LocalDate day;
-	private LocalDate month;
-	private LocalDate year;
+	private int day;
+	private int month;
+	private int year;
+	private String monthMMM;
 	DateTimeFormatter format1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 	DateTimeFormatter format2 = DateTimeFormatter.ofPattern("MMMM dd,yyyy");
 	DateTimeFormatter format3 = DateTimeFormatter.ofPattern("DDD yyyy");
 	
 	MyDate(int month, int day, int year){
+		this.day = day;
+		this.month = month - 1;
+		this.year = year;
+		LocalDate date = LocalDate.of(year, month, day);
+		
+		System.out.println("MM/dd/yyyy: " +date.format(format1));
+		System.out.println("MMMM dd,yyyy: " +date.format(format2));
+		System.out.println("DDD yyyy: " +date.format(format3));
 		
 	}
 	MyDate(String month, int day, int year ){
-		//this.month = LocalDate.
+		monthMMM = month.toUpperCase();
+		this.day = day;
+		this.year = year;
+		
+		LocalDate date = LocalDate.of(year, Month.valueOf(monthMMM),day);
+		System.out.println("MM/dd/yyyy: " +date.format(format1));
+		System.out.println("MMMM dd,yyyy: " +date.format(format2));
+		System.out.println("DDD yyyy: " +date.format(format3));
 	}
 	MyDate(int dayOfYear, int year ){
+		day = dayOfYear;
+		this.year = year;
+		
+		LocalDate date = LocalDate.ofYearDay(year,dayOfYear);
+		System.out.println("MM/dd/yyyy: " +date.format(format1));
+		System.out.println("MMMM dd,yyyy: " +date.format(format2));
+		System.out.println("DDD yyyy: " +date.format(format3));
 		
 	}
-    MyDate(){
-    	System.out.println("Nothing");
-    }
 	
 }
